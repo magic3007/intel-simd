f142804477b01005de240ad814df9c7f4a3f4bb4
magic3007 <maijing3007@qq.com>
2020-05-19 00:30:08 +0800

    [main] update

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 #include "CLI11.hpp"
 #include "config.h"
 #include "utils/base.h"
+#include "utils/stopwatch.hpp"
 #include "impl_simple.h"
 #include "impl_simd.h"
 #include "impl_mmx.h"
+#include "impl_sse2.h"
+#include "impl_avx.h"
 
 using std::string;
-
 void do_work(Impl &impl, YUV420 *yuv_src, YUV420 *yuv_dst, RGB *rgb,
              uint8_t alpha){
   impl.YUV2RGB(yuv_src, rgb);
@@ -19,8 +21,10 @@ void do_work(Impl &impl, YUV420 *yuv_src, YUV420 *yuv_dst, RGB *rgb,
 int main(int argc, char *argv[]){
   CLI::App app("Convert YUV420 file to RGB");
   std::string fileName = "default";
+  bool should_dump = false;
   app.add_option("-f", fileName, "Input YUV420 file")
       -> required();
+  app.add_option("-s", should_dump, "Whether to dump the output");
   CLI11_PARSE(app, argc, argv);
 
   YUV420 yuv_src = YUV420(kWidth, kHeight);
@@ -32,19 +36,30 @@ int main(int argc, char *argv[]){
 
   yuv_src.ReadFromYUVFile(fileName);
 
-  ImplSimple simple;
+  ImplSimple simple_isa;
   ImplSimd<SimdMMX> mmx;
+  ImplSimd<SimdSSE2> sse2;
+  ImplSimd<SimdAVX> avx;
 
   #define LIST_OF_IMPL \
-    X(mmx)
+    X(simple_isa) \
+    X(mmx) \
+    X(sse2) \
+    X(avx)
 
   #define X(name) try{ \
+    stopwatch::Stopwatch stopwatch; \
+    stopwatch.start(); \
     for (size_t alpha = 0; alpha < 255; alpha += 3) { \
       do_work(name, &yuv_src, &yuv_dst, &rgb, alpha); \
-      string nameString = string(#name); \
-      string outFile = nameString + "_output_" + std::to_string(alpha) + ".yuv"; \
-      yuv_dst.WriteToYUVFile(outFile); \
+      if (should_dump){ \
+        string nameString = string(#name); \
+        string outFile = nameString + "_output_" + std::to_string(alpha) + ".yuv"; \
+        yuv_dst.WriteToYUVFile(outFile); \
+      } \
     } \
+    std::cout << std::left << std::setw(10) << #name << ": " << std::setw(8) \
+      << std::right << stopwatch.elapsed<stopwatch::ms>() << "ms" << std::endl; \
   }catch (const std::exception& e) { \
     std::cerr << #name << ": " << e.what() << std::endl; \
   }
