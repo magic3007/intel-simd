629e5ee9aa7c435cb6c2a0fb99588199917da63b
magic3007 <maijing3007@qq.com>
2020-05-19 00:28:31 +0800

    [cmake/FindSSE] add "AVX2"

--- cmake/FindSSE.cmake
+++ cmake/FindSSE.cmake
@@ -24,10 +24,20 @@ if (CMAKE_SYSTEM_NAME MATCHES "Linux")
   else ()
     set(AVX_FOUND false CACHE BOOL "AVX available on host")
   endif ()
+
+  string(REGEX REPLACE "^.*(avx2).*$" "\\1" SSE_THERE ${CPUINFO})
+  string(COMPARE EQUAL "avx2" "${SSE_THERE}" AVX2_TRUE)
+  if (AVX2_TRUE)
+    set(AVX2_FOUND true CACHE BOOL "AVX2 available on host")
+  else ()
+    set(AVX2_FOUND false CACHE BOOL "AVX2 available on host")
+  endif ()
+
 else ()
   set(MMX_FOUND    true  CACHE BOOL "MMX available on host")
   set(SSE2_FOUND   true  CACHE BOOL "SSE2 available on host")
   set(AVX_FOUND    false CACHE BOOL "AVX available on host")
+  set(AVX2_FOUND   false CACHE BOOL "AVX2 available on host")
 endif ()
 
 if (NOT MMX_FOUND)
@@ -48,6 +58,12 @@ else()
     message(STATUS "AVX is available on host")
 endif ()
 
+if (NOT AVX2_FOUND)
+  message(STATUS "Could not find hardware support for AVX2 on this machine.")
+else()
+    message(STATUS "AVX2 is available on host")
+endif ()
+
 set(SSE_COMPILER_FLAGS )
 
 if (CMAKE_SYSTEM_NAME MATCHES "Linux")
@@ -61,6 +77,10 @@ if (CMAKE_SYSTEM_NAME MATCHES "Linux")
 
     if (AVX_FOUND)
         set(SSE_COMPILER_FLAGS "${SSE_COMPILER_FLAGS} -mavx")
+    endif ()
+
+    if (AVX2_FOUND)
+        set(SSE_COMPILER_FLAGS "${SSE_COMPILER_FLAGS} -mavx2")
     endif ()
 endif ()
 
